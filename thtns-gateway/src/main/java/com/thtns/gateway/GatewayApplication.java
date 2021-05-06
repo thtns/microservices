bbd0fad4be729bea97f01ebbfee8855284f967c8
liuyujun <gaea_it@163.com>
2021-05-06 23:27:07 +0800

    gateway

---
+++ thtns-gateway/src/main/java/com/thtns/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+
+package com.thtns.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lengleng
+ * @date 2018年06月21日
+ * <p>
+ * 网关应用
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
