b15d8babdbf6e67fc8a3f2c798ff25d27fb62919
LiuYujun <liuyujun@ifugle.com>
2021-05-06 19:15:38 +0800

    init.

---
+++ thtns-admin/src/main/java/com/thtns/admin/AdminApplication.java
@@ -0,0 +1,10 @@
+package com.thtns.admin;
+
+/**
+ * @author liuyj
+ * @description
+ * @date 2021/5/6
+ */
+
+public class AdminApplication {
+}
---
+++ thtns-auth/src/main/java/com/thtns/auth/AuthApplication.java
@@ -0,0 +1,10 @@
+package com.thtns.auth;
+
+/**
+ * @author liuyj
+ * @description
+ * @date 2021/5/6
+ */
+
+public class AuthApplication {
+}
